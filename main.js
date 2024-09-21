04c63138ec58ed332b6fb6bf895e853c20bb7043
svnderhvrtog <sander-hartog@outlook.com>
2024-09-21 16:01:07 +0200

    cleaned up the files especially css and added button motion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Copied from source for collapse menu
+// Collapse menu (code written by Takuya Matsuyama)
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -13,3 +13,21 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+// Mouse effect (code written by Maxim Verhoef)
+var targetIsCursor = (s, o = document) => o.querySelector(s);
+var targetIsButton = (s, o = document) => o.querySelectorAll(s);
+
+targetIsButton('.magnetic').forEach(el => el.addEventListener('mousemove', function(e) {
+  var pos = this.getBoundingClientRect();
+  var mx = e.clientX - pos.left - pos.width/2; 
+  var my = e.clientY - pos.top - pos.height/2;
+   
+  this.style.transform = 'translate('+ mx * 0.15 +'px, '+ my * 0.3 +'px)';
+  this.children[0].style.transform = 'translate('+ mx * 0.025 +'px, '+ my * 0.075 +'px)';
+}));
+
+targetIsButton('.magnetic').forEach(el => el.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+}));
