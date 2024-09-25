e67ef47df2f0f362550713ddf44322786d6c6b94
svnderhvrtog <sander-hartog@outlook.com>
2024-09-25 10:58:29 +0200

    all content items are finished and all documents are cleaned up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Background randomizer
-const colors = ['#e7ec6816', '#e9494918', '#15fa8b1c', '#628dd238', '#b880c425', '#8a702924', '#9595952c', '#95c88314', '#564c6814', '#20232014'];
+const colors = ['#e7ec6816', '#e9494918', '#15fa8b1c', '#628dd238', '#b880c425', '#8a702924', '#9595952c', '#95c88314', '#564c6814', '#20232014', '#19d96628', '#5fdc5b30', '#3857c81c'];
 
 function setBg() {
-    document.body.style.backgroundColor = colors[Math.floor(10*Math.random())];
+    document.body.style.backgroundColor = colors[Math.floor(14*Math.random())];
 }
