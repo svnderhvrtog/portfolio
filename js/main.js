32de9229ea281b1bc5a85d6b54a7be5c1e6b5779
svnderhvrtog <sander-hartog@outlook.com>
2024-09-27 12:53:48 +0200

    additional changes nothing big

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 // Background randomizer
 const colors = ['#e7ec6816', '#e9494918', '#15fa8b1c', '#628dd238', '#b880c425', '#8a702924', '#9595952c', '#95c88314', '#564c6814', '#20232014', '#19d96628', '#5fdc5b30', '#3857c81c'];
 
-function setBg() {
+document.getElementById('backgrbtn').addEventListener('click', function setBg() {
     document.body.style.backgroundColor = colors[Math.floor(14*Math.random())];
+});
+
+// Tracker (nice for mixed bag)
+/* 
+HTML
+<div id="divBox"></div>
+<div id="divCoords"></div>
+
+CSS
+#divBox {
 }
+
+#divCoords {
+    position: sticky;
+    top: 0;
+    z-index: 99;
+    color: #5c5c5c43;
+    margin-top: 4px;
+} 
+
+JS
+window.addEventListener('load', function () {
+    const box = document.getElementById('divBox');
+    const coords = document.getElementById('divCoords');
+
+box.onmousemove = function (e) {
+    coords.innerHTML = 'Coordinaten: X : ' + e.clientX + ', Y: ' + e.clientY;
+};
+
+});
+
+*/
