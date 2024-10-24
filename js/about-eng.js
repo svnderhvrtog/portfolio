8f924986d46676da5542abf0660d861a570cfef2
svnderhvrtog <sander-hartog@outlook.com>
2024-10-24 17:15:32 +0200

    eventhough i finished the about-me pages, i wrote an even cleaner version that seperates the btns from html

--- js/about-eng.js
+++ js/about-eng.js
@@ -9,27 +9,64 @@ const introduction = document.getElementById('about-introduction');
 const question1 = document.getElementById('question1');
 const rightAnswer1 = document.getElementById('rightanswer1');
 const wrongAnswer1 = document.getElementById('wronganswer1');
+const rightButton1 = document.querySelector('.q1aCorrect').addEventListener('click', q1aCorrectFunct);
+const wrongButton1 = document.querySelectorAll('.q1aWrong');
+for(let i = 0; i < wrongButton1.length; i++){
+    wrongButton1[i].addEventListener('click', q1aWrongFunct)
+};
+const nextButton1 = document.querySelectorAll('.q1Next');
+for(let i = 0; i < nextButton1.length; i++){
+    nextButton1[i].addEventListener('click', q1NextQuestionFunct)
+};
 
 // Question 2 variables
 const question2 = document.getElementById('question2');
 const rightAnswer2 = document.getElementById('rightanswer2');
 const wrongAnswer2 = document.getElementById('wronganswer2');
+const rightButton2 = document.querySelector('.q2aCorrect').addEventListener('click', q2aCorrectFunct);
+const wrongButton2 = document.querySelectorAll('.q2aWrong');
+for(let i = 0; i < wrongButton2.length; i++){
+    wrongButton2[i].addEventListener('click', q2aWrongFunct)
+};
+const nextButton2 = document.querySelectorAll('.q2Next');
+for(let i = 0; i < nextButton2.length; i++){
+    nextButton2[i].addEventListener('click', q2NextQuestionFunct)
+};
 
 // Question 3 variables
 const question3 = document.getElementById('question3');
 const rightAnswer3 = document.getElementById('rightanswer3');
 const wrongAnswer3 = document.getElementById('wronganswer3');
+const rightButton3 = document.querySelector('.q3aCorrect').addEventListener('click', q3aCorrectFunct);
+const wrongButton3 = document.querySelectorAll('.q3aWrong');
+for(let i = 0; i < wrongButton3.length; i++){
+    wrongButton3[i].addEventListener('click', q3aWrongFunct)
+};
+const nextButton3 = document.querySelectorAll('.q3Next');
+for(let i = 0; i < nextButton3.length; i++){
+    nextButton3[i].addEventListener('click', q3NextQuestionFunct)
+};
 
 // Question 4 variables
 const question4 = document.getElementById('question4');
 const rightAnswer4 = document.getElementById('rightanswer4');
 const wrongAnswer4 = document.getElementById('wronganswer4');
+const rightButton4 = document.querySelector('.q4aCorrect').addEventListener('click', q4aCorrectFunct);
+const wrongButton4 = document.querySelectorAll('.q4aWrong');
+for(let i = 0; i < wrongButton4.length; i++){
+    wrongButton4[i].addEventListener('click', q4aWrongFunct)
+};
+const nextButton4 = document.querySelectorAll('.q4Next');
+for(let i = 0; i < nextButton4.length; i++){
+    nextButton4[i].addEventListener('click', continueFunct)
+};
 
 // Score variables
 const aboutFinal = document.getElementById('about-final');
 const totalScore = document.getElementById('score');
 let scoringText = document.getElementById('scoringtext');
 let points = 0;
+const returnButton = document.querySelector('.return').addEventListener('click', returnFunct);
 
 
 // Begin Quiz function
