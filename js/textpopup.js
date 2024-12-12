a394a49724f7c5c35e4d898a6850c77a5c8b6f96
svnderhvrtog <sander-hartog@outlook.com>
2024-12-12 20:06:57 +0100

    worked on casino case and renewed the so-called prototype images with click me banner

--- js/textpopup.js
+++
@@ -1,9 +0,0 @@
-// Text popup
-const popupContent = document.getElementById('popupContent');
-
-document.getElementById('popupContainer').addEventListener('mouseover', () => {
-    popupContent.style.display = 'block';
-});
-document.getElementById('popupContainer').addEventListener('mouseout', () => {
-    popupContent.style.display = 'none';
-});
